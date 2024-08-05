7120cabfe020eb17963af27fccce7ba7197c1ea1
sahayakegin <sahayakegin4@gmail.com>
2024-08-05 19:05:50 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+/*whithout function
+var a =10;
+var b =20;
+var c =a+b;
+console.log(c);
+*/
+/*with function but with no returns
+function add(a, b) {
+  console.log(a + b);
+}
+
+function sub(a, b) {
+  console.log(a - b);
+}
+
+add(10, 20);
+sub(20, 10);
+*/
+function arithmetic(a,b){
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a%b);
+}
+arithmetic(20, 50)
